df8f04ea081b507bb24d3b22c65c816faf0b2f76
jimmyberg <jimmyberg.kim@gmail.com>
2022-12-11 01:06:11 +0900

    project initialize
    - 프로젝트 기본 환경 구성

---
+++ src/main/java/me/jimmyberg/wanted/WantedApiServiceApplication.java
@@ -0,0 +1,13 @@
+package me.jimmyberg.wanted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantedApiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WantedApiServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/jimmyberg/wanted/WantedApiServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package me.jimmyberg.wanted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WantedApiServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("Hello Wanted");
+    }
+
+}
