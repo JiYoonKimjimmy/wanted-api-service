d4343dec907c50308a1646e01d3bc8e89935dd74
jimmyberg <jimmyberg.kim@gmail.com>
2022-12-12 01:26:13 +0900

    spring security 적용
    - security 기본 config class 추가

---
+++ src/main/java/me/jimmyberg/wanted/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package me.jimmyberg.wanted.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+
+@Configuration
+public class SecurityConfig implements AuthenticationProvider {
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        // API 인증 절차가 필요한 경우, 해당 함수에서 구현..
+        System.out.println("authentication.getName() = " + authentication.getName());
+        System.out.println("authentication.getCredentials() = " + authentication.getCredentials());
+        return authentication;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return true;
+    }
+
+}
