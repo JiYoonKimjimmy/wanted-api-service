a8a3e75c9ab88613a1a608dcb84e5902bdf9561f
jimmyberg <jimmyberg.kim@gmail.com>
2022-12-12 02:58:59 +0900

    spring security 임시 제거

--- src/main/java/me/jimmyberg/wanted/config/SecurityConfig.java
+++ src/main/java/me/jimmyberg/wanted/config/SecurityConfig.java
@@ -1,24 +1,37 @@
-package me.jimmyberg.wanted.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-
-@Configuration
-public class SecurityConfig implements AuthenticationProvider {
-
-    @Override
-    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        // TODO API 인증 절차가 필요한 경우, 해당 함수 구현
-        System.out.println("authentication.getName() = " + authentication.getName());
-        System.out.println("authentication.getCredentials() = " + authentication.getCredentials());
-        return authentication;
-    }
-
-    @Override
-    public boolean supports(Class<?> authentication) {
-        return true;
-    }
-
-}
+//package me.jimmyberg.wanted.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.AuthenticationProvider;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.AuthenticationException;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//import static org.springframework.security.config.Customizer.withDefaults;
+//
+//@Configuration
+//public class SecurityConfig implements AuthenticationProvider {
+//
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        return http
+//                .authorizeHttpRequests((auth) -> auth.anyRequest().permitAll())
+//                .httpBasic(withDefaults())
+//                .build();
+//    }
+//
+//    @Override
+//    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+//        // TODO API 인증 절차가 필요한 경우, 해당 함수 구현
+//        System.out.println("authentication.getName() = " + authentication.getName());
+//        System.out.println("authentication.getCredentials() = " + authentication.getCredentials());
+//        return authentication;
+//    }
+//
+//    @Override
+//    public boolean supports(Class<?> authentication) {
+//        return true;
+//    }
+//
+//}
