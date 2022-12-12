0742b91d7bcd1d8d97a601e22fd5ac8977ab81ad
jimmyberg <jimmyberg.kim@gmail.com>
2022-12-12 18:40:30 +0900

    hello 관련 서비스 제거
    - test 코드 정리

--- src/main/java/me/jimmyberg/wanted/api/v0/hello/HelloRepository.java
+++
@@ -1,6 +0,0 @@
-package me.jimmyberg.wanted.api.v0.hello;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HelloRepository extends JpaRepository<Hello, Long>, HelloQRepository {
-}
