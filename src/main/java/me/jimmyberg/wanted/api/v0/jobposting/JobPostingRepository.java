56a47aad2f4ae99647f9c537443fe3efe2f45b59
jimmyberg <jimmyberg.kim@gmail.com>
2022-12-14 01:53:15 +0900

    job posting 제거

--- src/main/java/me/jimmyberg/wanted/api/v0/jobposting/JobPostingRepository.java
+++
@@ -1,6 +0,0 @@
-package me.jimmyberg.wanted.api.v0.jobposting;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JobPostingRepository extends JpaRepository<JobPosting, String> {
-}
